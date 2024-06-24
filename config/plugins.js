b639d2c6b9810217329b9e65944a23227d15e9d2
Lee Webb <leewerbb55@hotmail.com>
2024-06-25 00:36:26 +0100

    added jwt

--- config/plugins.js
+++ config/plugins.js
@@ -1,10 +1,16 @@
 module.exports = ({ env }) => ({
     // ...
+    'users-permissions': {
+    config: {
+      jwtSecret: env('JWT_SECRET'),
+      },
+    },
     seo: {
       enabled: true,
     },
     redirects: {
         enabled: true,
       },
+
     // ...
   });
