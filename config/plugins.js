0a8848563547d233af81b994e8745f7a10ad9ff9
Lee Webb <leewerbb55@hotmail.com>
2024-06-24 23:45:46 +0100

    updated with some plug-ins

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,10 @@
-module.exports = () => ({});
+module.exports = ({ env }) => ({
+    // ...
+    seo: {
+      enabled: true,
+    },
+    redirects: {
+        enabled: true,
+      },
+    // ...
+  });
